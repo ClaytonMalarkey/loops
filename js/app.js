7058c0f7808c9dea1447af23352f38c3464c845b
ClaytonMalarkey <malarkey1985@gmail.com>
2023-09-13 20:37:43 -0600

    third commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+var num = 0;
+
+while(num>=50){
+    console.log("num is " + num);
+    num ++;
+}
+num = 0;
+while(num>=-50){
+    console.log("num is " + num);
+    num --;
+}
+num = 0;
+while(num<=100){
+    if(num % 2 == 0){
+        console.log("even number " + num);
+    }
+    num ++;
+}
